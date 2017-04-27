19b4a25b76b74ec396401597b3756e97033b56de
Andrew Krawchyk <akrawchyk@gmail.com>
2017-04-26 22:05:14 -0400

    add interest and profile, fix procfile

--- cityguide/users/admin.py
+++ cityguide/users/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import Interest, Profile, User
 
 
+class InterestAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Interest, InterestAdmin)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(User, UserAdmin)
--- cityguide/users/models.py
+++ cityguide/users/models.py
@@ -2,5 +2,23 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class Interest(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Profile(models.Model):
+    age = models.IntegerField
+    location = models.CharField(max_length=64)
+    discretionary_income = models.IntegerField()
+    miles_willing_to_travel = models.IntegerField()
+    # mobility
+    group_person_count = models.IntegerField()
+    interests = models.ManyToManyField(Interest)
+
+
 class User(AbstractUser):
-    pass
+    profile = models.OneToOneField(
+        Profile,
+        on_delete=models.CASCADE,
+        null=True
+    )
